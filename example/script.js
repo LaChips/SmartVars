ae5053bd8740a1aaf696baf9b718f9bb28f5081d
Titouan Le Floch Riche <titouan.leflochriche@outlook.com>
2021-04-30 04:49:07 +0200

    Example updated

--- example/script.js
+++ example/script.js
@@ -29,19 +29,19 @@ var names = [
   "Simon"
 ]
 
-btn.addEventListener('click', function() {
-  // Updating SmartVars
-  SmartVars["test"].value.set("ok");
-  
-  // Updating SmartObjs
 
-  // You can update specific fields independently
-  SmartObjs["person"].value.set(names[Math.ceil(Math.random() * names.length - 1)], "n");
-  SmartObjs["person"].value.set(Math.floor(Math.random() * 100), "age");
-  SmartObjs["person"].value.set("6 rue Marc Sangnier", "person_adress.street");
-  SmartObjs["person"].value.set("Vanves", "person_adress.city");
-  SmartObjs["person"].value.set("France", "person_adress.country");
-  
-  // Or update the whole object
-  SmartObjs["person"].value.set({n: names[Math.ceil(Math.random() * names.length - 1)], age: Math.floor(Math.random() * 100), person_adress: {street: "test 1", city: "test 2", country: "test 3"}});
+btn.addEventListener('click', function() {
+  var request_data = {
+    n: names[Math.ceil(Math.random() * names.length - 1)],
+    age: Math.floor(Math.random() * 100),
+    person_adress: {
+      street: "test 1",
+      city: "test 2",
+      country: "test 3"
+    }
+  };
+  // Simulate example request reponse after 1 second using the data above
+  setTimeout(function(/* request_data */) {
+    SmartObjs.person.value.set(request_data);
+  }, 1000);
 });
